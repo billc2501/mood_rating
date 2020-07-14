341d7c861dfaffb3ee0f7986ce5b2fc58b769f73
billc2501 <billc2501@gmail.com>
2020-07-14 11:00:13 -0700

    server for rating company finish

--- file.js
+++ file.js
@@ -1,22 +1,35 @@
+var companyratings = {
+    "customer service" : 10,
+    "computer science" : 9,
+}
+
 console.log('server is starting')
-
 var express = require('express');
-
 var item = express();
-
 var port = item.listen(3000);
 
+
 item.use(express.static('site'));
+item.get('/rate/:department/:score', rate)
+item.get("/ratings", all);
+item.get('/profile/:person/:page?', piece);
 
-
-item.get('/search/:item1/:num', piece);
+function rate(request, response){
+    var items = request.params;
+    var department = items.department;
+    var value = Number(items.score);
+    companyratings[department] = value;
+    var message = {msg :"Thanks for rating the " + String(department) + " department"};
+    response.send(message);
+}
+function all(request, response){
+    response.send(companyratings);
+}
 function piece(request, response){
     var data = request.params;
-    var num = data.num;
-    str = ""
-    for (let i = 0; i < num; i ++)
-    {
-        str += "Are you searching for "+ String(data.item1) + "\n";
-    }
+    var num = data.page;
+    var letter = String(data.person[0]).toUpperCase();
+    var name = letter + String(data.person).substr(1, data.person.length);
+    str = "Are you looking for page " + String(num) + " of " + String(name) + "'s profile? \n";
     response.send(str);
 }
